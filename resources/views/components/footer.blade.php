d7ab0e9030a034fc8ef68adb61d41ff3c3219bae
_azammm <apple@MacBook-Pro-apple.local>
2026-02-05 00:48:19 +0700

    :lipstick: Add UI Footer Component

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,3 +1,3 @@
-<div class="absolute shadow bottom-0 w-full py-6 text-center bg-white">
+<div class="shadow w-full py-6 text-center bg-white">
     <p class="text-sm text-[#4A5565]">© 2026 CrowdfundingLite. Campaign and donation management platform prototype.</p>
 </div>
